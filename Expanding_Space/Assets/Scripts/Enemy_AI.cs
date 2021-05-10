5a0e14424b42c98d7e726c5b6f12ccecece58631
yairme <yairstward2013@gmail.com>
2021-05-10 17:52:45 +0200

    Fixing attempt 1.

---
+++ Expanding_Space/Assets/Scripts/Enemy_AI.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AI : MonoBehaviour
+{
+    //Base class/AI for the enemies.
+    protected float HP = 100f;
+    protected float speed = 5f;
+    protected float value = 1f;
+
+    protected Transform target;
+    protected int wavepointIndex = 0;
+
+    private void Start()
+    {
+        target = WayPoints.wpoints[0];
+    }
+
+    private void Update()
+    {
+        Vector2 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector2.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= WayPoints.wpoints.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        wavepointIndex++;
+        target = WayPoints.wpoints[wavepointIndex];
+    }
+}
---
+++ Expanding_Space/Assets/Scripts/WayPoints.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoints : MonoBehaviour
+{
+    public static Transform[] wpoints;
+
+    private void Awake()
+    {
+        wpoints = new Transform[transform.childCount];
+        for (int i = 0; i < wpoints.Length; i++)
+        {
+            wpoints[i] = transform.GetChild(i);
+        }
+    }
+
+}
