e3c1ae7b7ef773f48e895c80062e05d0d7f4ca3e
BuildTools <unconfigured@null.spigotmc.org>
2021-05-11 14:06:45 +0200

    start shop

---
+++ shop/Assets/scripts/Button.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [HideInInspector] public Inventory inv;
+    public GameObject shopmanager;
+
+    public void Start()
+    {
+        inv = shopmanager.GetComponent<Inventory>();
+
+    }
+
+
+    public void buy()
+    {
+        Debug.Log("bought");
+        if(inv.Points > 0)
+        {
+            inv.Points -= 5;
+            Debug.Log(inv.Points);
+        }
+        else
+        {
+            Debug.Log("no points");
+        }
+    }
+
+
+
+
+}
---
+++ shop/Assets/scripts/shop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shop : MonoBehaviour
+{
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+
+
+
+
+
+
+
+}
