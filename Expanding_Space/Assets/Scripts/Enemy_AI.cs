5c2d23e7ef084c24cc6d55d97e8a6612a171f5e2
yairme <yairstward2013@gmail.com>
2021-06-14 12:30:38 +0200

    Fixing more merge issues with the enemy AI.

--- Expanding_Space/Assets/Scripts/Enemy_AI.cs
+++ Expanding_Space/Assets/Scripts/Enemy_AI.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
 using UnityEngine.UI;
 
 public class Enemy_AI : MonoBehaviour
@@ -20,32 +19,22 @@ public class Enemy_AI : MonoBehaviour
 
     [Header("Unity stuff")]
     public Image healthBar;
-=======
-
-public class Enemy_AI : MonoBehaviour
-{
-    //Base class/AI for the enemies.
-    protected float HP = 100f;
-    protected float speed = 5f;
-    protected float value = 1f;
->>>>>>> master
 
     protected Transform target;
     protected int wavepointIndex = 0;
 
     private void Start()
     {
-<<<<<<< HEAD
         speed = startSpeed;
         target = WayPoints.wpoints[0];
     }
 
-    public void TakeDamage (int amount)
+    public void TakeDamage(int amount)
     {
         health -= amount;
 
         healthBar.fillAmount = health / startHealth;
-        
+
         if (health <= 0)
         {
             Die();
@@ -59,17 +48,11 @@ public class Enemy_AI : MonoBehaviour
         Destroy(gameObject);
     }
 
-=======
-        target = WayPoints.wpoints[0];
-    }
-
->>>>>>> master
     private void Update()
     {
         Vector2 dir = target.position - transform.position;
         transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
 
-<<<<<<< HEAD
         if (Vector2.Distance(transform.position, target.position) <= 0.4f)
         {
             GetNextWaypoint();
@@ -78,22 +61,18 @@ public class Enemy_AI : MonoBehaviour
         Vector3 dirL = WayPoints.wpoints[wavepointIndex].position - transform.position;
         float angle = Mathf.Atan2(dirL.y, dirL.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-    }
 
-
-=======
         if (Vector2.Distance(transform.position, target.position) <= 0.2f)
         {
             GetNextWaypoint();
         }
     }
 
->>>>>>> master
     void GetNextWaypoint()
     {
+
         if (wavepointIndex >= WayPoints.wpoints.Length - 1)
         {
-<<<<<<< HEAD
             EndPath();
             return;
         }
@@ -109,13 +88,3 @@ public class Enemy_AI : MonoBehaviour
         Destroy(gameObject);
     }
 }
-
-
-=======
-            Destroy(gameObject);
-        }
-        wavepointIndex++;
-        target = WayPoints.wpoints[wavepointIndex];
-    }
-}
->>>>>>> master
