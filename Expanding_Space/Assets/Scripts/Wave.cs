a7e7c7e7ec8a11241f4025179b048e3d151c0622
yairme <yairstward2013@gmail.com>
2021-06-21 11:15:35 +0200

    Update to wave spawner

--- Expanding_Space/Assets/Scripts/Wave.cs
+++ Expanding_Space/Assets/Scripts/Wave.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Wave 
+public class Wave
 {
-    public GameObject enemy;
+    [Header("Enemy stats")]
     public int count;
     public float rate;
+
+    [Header("Enemy count/type")]
+    public GameObject[] enemy;
 }
