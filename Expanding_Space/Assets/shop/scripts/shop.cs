b2fa034cea39133e714e93714b1d1c0b6f51bdf9
yairme <yairstward2013@gmail.com>
2021-06-23 12:46:54 +0200

    Merging

---
+++ Expanding_Space/Assets/shop/scripts/shop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class shop : MonoBehaviour
+{
+    private bool standard;
+    private bool laser;
+    private bool missle;
+    [HideInInspector]public GameObject turret;
+    public GameObject dashop;
+    private GameObject grid;
+
+    buildmanager Buildmanager;
+
+    private void Start()
+    {
+        Buildmanager = buildmanager.instance;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            dashop.SetActive(true);
+        }
+    }
+    public void placestandard()
+    {
+        Buildmanager.SetTurretToBuild(Buildmanager.standardturret);
+    }
+    public void placelaser()
+    {
+        Buildmanager.SetTurretToBuild(Buildmanager.laserturret);
+    }
+    public void placemissle()
+    {
+        Buildmanager.SetTurretToBuild(Buildmanager.Missleturret);
+    }
+}
